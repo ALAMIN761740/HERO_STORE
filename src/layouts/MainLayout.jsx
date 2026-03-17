9488410483432b65b91a8914d7d1d8a3b650d097
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-03-18 01:07:03 +0600

    feat(footer): responsive layout and social icons

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer"
 
 const MainLayout = () => {
   return (
@@ -9,7 +9,7 @@ const MainLayout = () => {
       <div className="min-h-[80vh]">
         <Outlet />
       </div>
-      {/* <Footer /> */}
+      <Footer /> 
     </>
   );
 };
