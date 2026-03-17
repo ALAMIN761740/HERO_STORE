45eb0bd0b165985655a4569ea67b3c699f2cfa5e
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-03-18 00:44:32 +0600

    feat(navbar): inline logo and name, add GitHub icon, fix imports/styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,51 @@
-import { NavLink } from "react-router";
+import { NavLink  } from "react-router";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
+
+
+
+    // Function to determine the class for active and inactive links
+    const navClass = ({ isActive }) =>
+    `relative  font-semibold pb-1 transition-all ${
+        isActive
+        ? "text-transparent bg-clip-text bg-gradient-to-br from-[#632EE3] to-[#9F62F2] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[2px] after:bg-gradient-to-br after:from-[#632EE3] after:to-[#9F62F2]"
+        : "text-black hover:text-gray-500"
+    }`
+    ;
+
+
+
   return (
-    <nav className="flex justify-between p-4 shadow">
-      <NavLink to="/">Hero IO</NavLink>
+    <nav className="flex justify-between p-4  shadow">
+      <NavLink to="/" className="inline-flex items-center space-x-2">
+        <img className="w-8 h-8" src="./../../assets/logo.png" alt="Hero Store logo" />
+        <span className="text-lg font-semibold bg-gradient-to-br from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">Hero Store</span>
+      </NavLink>
 
-      <div className="space-x-4">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/apps">Apps</NavLink>
-        <NavLink to="/installation">Installation</NavLink>
-      </div>
+      <div className="space-x-6">
+            <NavLink to="/" className={navClass}>
+                Home
+            </NavLink>
 
-      <a href="https://github.com/your-profile">Contribution</a>
+            <NavLink to="/apps" className={navClass}>
+                Apps
+            </NavLink>
+
+            <NavLink to="/installation" className={navClass}>
+                Installation
+            </NavLink>
+    </div>
+
+      <a
+        href="https://github.com/ALAMIN761740"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-flex items-center space-x-2 bg-gradient-to-br from-[#632EE3] to-[#9F62F2] text-white px-4 py-2 rounded"
+      >
+        <FaGithub />
+        <span>Contribution</span>
+      </a>
     </nav>
   );
 };
