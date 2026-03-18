f37a0ac3f3e70cf7295f882a0be7a4639de90900
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-03-18 10:41:12 +0600

    Add ErrorPage component

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router";
+import errorImg from "./../../assets/error-404.png"; 
+
 
 const ErrorPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center px-4">
-      <h1 className="text-7xl font-bold text-red-500">404</h1>
-      <p className="text-2xl mt-4 font-semibold">Page Not Found</p>
-      <p className="text-gray-500 mt-2">
-        Oops! The page you are looking for does not exist.
-      </p>
+    <div className="  flex flex-col min-h-screen bg-[#f8fafc]">
+      
+      <div className="flex flex-col items-center justify-center flex-grow text-center px-4">
+        <img
+          src={errorImg}
+          alt="404 Error"
+          className="w-[300px] md:w-[300px] mb-6"
+        />
 
-      <Link
-        to="/"
-        className="mt-6 px-6 py-3 bg-gradient-to-r from-purple-600 to-indigo-600 text-white rounded-lg shadow hover:scale-105 transition"
-      >
-        Go Back Home
-      </Link>
+        <h1 className="text-3xl md:text-5xl font-bold text-gray-800">
+          Oops, page not found!
+        </h1>
+
+        <p className="text-gray-500 mt-3 max-w-md">
+          The page you are looking for is not available.
+        </p>
+
+        <Link
+          to="/"
+          className="mt-6 px-6 py-3 rounded-lg text-white font-medium
+          bg-gradient-to-r from-[#6C3BFF] to-[#9F62F2]
+          hover:scale-105 transition duration-300 shadow-lg"
+        >
+          Go Back!
+        </Link>
+      </div>
+      
     </div>
   );
 };
